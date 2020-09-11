6efdfe5f052d43d6edfd8604d05d04fd2ebf6336
robert <robert5638@gmail.com>
2020-09-11 15:50:32 -0400

    'updated'

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,14 @@
 module.exports = {
+  // database: {
+  //   host: 'localhost',
+  //   user: 'root',
+  //   password: '',
+  //   database: 'database_games'
+  // }
   database: {
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'database_games'
+    user: '3CAlTeQrpb',
+    password: 'VJmprbt29h',
+    database: '3CAlTeQrpb'
   }
 }
