e2bf4e93373486d0f22261a68229a83ab6cabe25
robert <robert5638@gmail.com>
2020-09-11 17:06:18 -0400

    '!how'

--- src/keys.js
+++ src/keys.js
@@ -1,14 +1,8 @@
 module.exports = {
-  // database: {
-  //   host: 'localhost',
-  //   user: 'root',
-  //   password: '',
-  //   database: 'database_games'
-  // }
   database: {
-    host: 'remotemysql.com',
-    user: '3CAlTeQrpb',
-    password: 'VJmprbt29h',
-    database: '3CAlTeQrpb'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'database_games'
   }
 }
